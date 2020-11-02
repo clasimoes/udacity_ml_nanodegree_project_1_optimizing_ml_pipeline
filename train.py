5311de057e93839cd03d814edee86b08fff1c586
Clarisse Simoes Ribeiro <clasimoesr@gmail.com>
2020-11-02 18:21:32 +0000

    Updated script after filling up blank TODO code
    $ python train.py --C 0.5 --max_iter=10000
    Attempted to log scalar metric Regularization Strength::
    0.5
    Attempted to log scalar metric Max iterations::
    10000
    Attempted to log scalar metric Accuracy:
    0.9117855336368235

--- train.py
+++ train.py
@@ -9,20 +9,10 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Dataset, Datastore
+from azureml.data.datapath import DataPath
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,9 +39,25 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df, y_df
     
 
 def main():
+    # TODO: Create TabularDataset using TabularDatasetFactory
+    # Data is located at:
+    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+    ds = Dataset.Tabular.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+
+    x, y = clean_data(ds)
+
+    # TODO: Split data into train and test sets.
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+    run = Run.get_context()
+
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
